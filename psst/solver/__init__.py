c07ede5f1066f067d6a9bcbd9d1cebd91d336945
Jeremy Zucker <djinnome@gmail.com>
2020-06-08 12:31:14 -0700

    Changed unfriendly overloading of solver parameter to separate solver_factory object

--- psst/solver/__init__.py
+++ psst/solver/__init__.py
@@ -10,23 +10,24 @@ PSST_WARNING = os.getenv('PSST_WARNING', 'ignore')
 
 def solve_model(model, solver='glpk', solver_io=None, keepfiles=True, verbose=True, symbolic_solver_labels=True, is_mip=True, **kwargs):
     if solver == 'xpress':
-        solver = SolverFactory(solver, solver_io=solver_io, is_mip=is_mip)
+        solver_factory = SolverFactory(solver, solver_io=solver_io, is_mip=is_mip)
     else:
-        solver = SolverFactory(solver, solver_io=solver_io)
+        solver_factory = SolverFactory(solver, solver_io=solver_io)
     model.preprocess()
     if is_mip and solver=='scip':
         if 'solver_options' in kwargs:
             if 'limits/gap' in kwargs['solver_options']:
-                solver.options['limits/gap']=float(kwargs['solver_options']['limits/gap'])
+                solver_factory.options['limits/gap']=float(kwargs['solver_options']['limits/gap'])
 
     if solver=='gurobi':
+        print('Kwargs: ', kwargs)
         if 'solver_options' in kwargs:
             if 'MIPGap' in kwargs['solver_options']:
-                solver.options['MIPGap'] = float(kwargs['solver_options']['MIPGap'])
+                solver_factory.options['MIPGap'] = float(kwargs['solver_options']['MIPGap'])
             if 'TimeLimit' in kwargs['solver_options']:
-                solver.options['TimeLimit'] = float(kwargs['solver_options']['TimeLimit'])
+                solver_factory.options['TimeLimit'] = float(kwargs['solver_options']['TimeLimit'])
     with warnings.catch_warnings():
         warnings.simplefilter(PSST_WARNING)
-        solver.solve(model, suffixes=['dual'], tee=verbose, keepfiles=keepfiles, symbolic_solver_labels=symbolic_solver_labels)
+        solver_factory.solve(model, suffixes=['dual'], tee=verbose, keepfiles=keepfiles, symbolic_solver_labels=symbolic_solver_labels)
 
     return model
